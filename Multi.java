220f256652c4d801590fcc5cc738078ec5cce782
jetullahi <jetullahhaziraj6@gmail.com>
2021-09-11 17:51:41 +0200

    java class

---
+++ Multi.java
@@ -0,0 +1,35 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Multi extends Thread{
+private Socket s=null;
+DataInputStream infromClient;
+Multi() throws IOException{
+
+
+}
+Multi(Socket s) throws IOException{
+    this.s=s;
+    infromClient = new DataInputStream(s.getInputStream());
+}
+public void run(){  
+
+    String SQL=new String();
+    try {
+        SQL = infromClient.readUTF();
+    } catch (IOException ex) {
+        Logger.getLogger(Multi.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    System.out.println("Query: " + SQL); 
+    try {
+        System.out.println("Socket Closing");
+        s.close();
+    } catch (IOException ex) {
+        Logger.getLogger(Multi.class.getName()).log(Level.SEVERE, null, ex);
+       }
+   }  
+}
