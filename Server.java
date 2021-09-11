6f886996d13665806187c135de045f2a7fb04a8a
jetullahi <jetullahhaziraj6@gmail.com>
2021-09-11 17:50:57 +0200

    java class

---
+++ Server.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+public static void main(String args[]) throws IOException, 
+InterruptedException{   
+
+    while(true){
+        ServerSocket ss=new ServerSocket(29523);
+        System.out.println("Server is Awaiting"); 
+        Socket s=ss.accept();
+        Multi t=new Multi(s);
+        t.start();
+
+        Thread.sleep(2000);
+        ss.close();
+    }    
+
+
+
+
+    }   
+}
